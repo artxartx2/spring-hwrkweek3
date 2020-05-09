7630da9e6f719a7875a3b526652ee798c08c37c3
Artur <artx@post.pl>
2020-05-09 15:16:29 +0200

    Initial commit

---
+++ spring-kurs/spring-hwrkweek3/src/main/java/pl/kurs/spring/hwrkweek3/Hwrkweek3Application.java
@@ -0,0 +1,13 @@
+package pl.kurs.spring.hwrkweek3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hwrkweek3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hwrkweek3Application.class, args);
+    }
+
+}
---
+++ spring-kurs/spring-hwrkweek3/src/test/java/pl/kurs/spring/hwrkweek3/Hwrkweek3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kurs.spring.hwrkweek3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hwrkweek3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
